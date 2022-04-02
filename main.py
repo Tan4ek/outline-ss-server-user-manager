1f4d73cf0e25fc35bc5cb6e8ad63f94a0ebae415
Sergei Samokhvalov <kolobok205@gmail.com>
2022-04-02 23:44:08 +0300

    get configs from envs, simple logging

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import logging
+import os
 import subprocess
 from dataclasses import dataclass
+from subprocess import TimeoutExpired
 from typing import List, Optional
 
 import yaml
@@ -8,11 +11,23 @@ from filelock import FileLock
 from pydantic import BaseModel
 from yaml import Loader
 
-# TODO: get from config or env or args
-SSPORT = 9000
-CONFIG_FILE_PATH = 'outline-ss-server/config.yml'
+logging.basicConfig(level='INFO',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+SSPORT = int(os.environ.get('SS_USER_PORT', 9000))
+CONFIG_FILE_PATH = os.environ.get('SS_CONFIG_PATH', 'outline-ss-server/config.yml')
+CIPHER = os.environ.get('SS_CIPHER', 'chacha20-ietf-poly1305')
+
+if CIPHER not in {'chacha20-ietf-poly1305', 'aes-256-gcm', 'aes-192-gcm', 'aes-128-gcm'}:
+    logger.error(f"Not valid SS_CIPHER: {CIPHER}. "
+                 f"Valid values: ['chacha20-ietf-poly1305', 'aes-256-gcm', 'aes-192-gcm', 'aes-128-gcm']")
+    import sys
+    sys.exit(1)
+
 CONFIG_FILE_PATH_LOCK = CONFIG_FILE_PATH + '.lock'
-CIPHER = 'chacha20-ietf-poly1305'
+
+logger.info(f"SSPORT: {SSPORT}, CONFIG_FILE_PATH: {CONFIG_FILE_PATH}, CIPHER: {CIPHER}")
 
 
 @dataclass
@@ -73,10 +88,16 @@ def find_user(state: State, user_id: str) -> Optional[UserConfig]:
 def restart_outline_ss_server():
     process = subprocess.Popen(['supervisorctl', 'restart', 'ss-server'],
                                stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    print(stdout)
-    print(stderr)
+                               stderr=subprocess.PIPE,
+                               universal_newlines=True)
+    try:
+        process.communicate(timeout=10)
+        logger.info("Success restart 'ss-server'.")
+    except TimeoutExpired as e:
+        process.kill()
+        logger.error(
+            f"Restart ss server timeout after 10 seconds. \n stdout: {process.stdout}, stderr: {process.stderr}", e)
+        raise e
 
 
 app = FastAPI()
@@ -95,22 +116,23 @@ def create_user(user: User, response: Response):
     if user.user_id in {x.id for x in state.users}:
         response.status_code = status.HTTP_409_CONFLICT
         return {}
-    user = UserConfig(
+    config_user = UserConfig(
         id=user.user_id,
         port=state.ssport,
         cipher=state.cipher,
         secret=user.sssecret
     )
     new_users = state.users.copy()
-    new_users.append(user)
+    new_users.append(config_user)
     new_state = State(ssport=state.ssport, cipher=state.cipher, users=new_users)
     save_state(new_state)
 
     restart_outline_ss_server()
+    logger.info(f"Create new user {config_user.id}")
     return {
-        'port': user.port,
-        'cipher': user.cipher,
-        'secret': user.secret
+        'port': config_user.port,
+        'cipher': config_user.cipher,
+        'secret': config_user.secret
     }
 
 
@@ -143,6 +165,7 @@ def delete_user(user_id: str, response: Response):
 
     restart_outline_ss_server()
 
+    logger.info(f"Delete user {user_id}")
     return {}
 
 
@@ -164,6 +187,3 @@ if __name__ == '__main__':
     s = get_state()
 
     save_state(s)
-    print(s)
-
-    pass
